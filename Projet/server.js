62ac464275a406066f85c5c1e26c1f4569c31cb8
pik <pikdev.gopi@gmail.com>
2025-03-18 15:15:46 +0100

    retest

--- Projet/server.js
+++ Projet/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Publication = require('./models/Publication');
+const Publication = require('./Node/models/Publication');
 const path = require('path');
 const fs = require('fs');
 
