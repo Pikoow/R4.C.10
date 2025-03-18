f4d1379c4761aea7ce132122afa2424c041a6a53
pik <pikdev.gopi@gmail.com>
2025-03-18 14:50:25 +0100

    update github pages

---
+++ Projet/fetchPublicationsJson.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const axios = require('axios');
+const bibtexParse = require('bibtex-parse');
+
+const latexToUnicode = {
+  "{\\'e}": "é",
+  "{\\`e}": "è",
+  "{\\^e}": "ê",
+  "{\\~n}": "ñ",
+  "{\\`a}": "à",
+  "{\\'a}": "á",
+  "{\\^a}": "â",
+  "{\\~a}": "ã",
+  "{\\`i}": "ì",
+  "{\\'i}": "í",
+  "{\\^i}": "î",
+  "{\\~i}": "ĩ",
+  "{\\`o}": "ò",
+  "{\\'o}": "ó",
+  "{\\^o}": "ô",
+  "{\\~o}": "õ",
+  "{\\`u}": "ù",
+  "{\\'u}": "ú",
+  "{\\^u}": "û",
+  "{\\~u}": "ũ",
+  "{\\c{c}}": "ç",
+  "{\\ss}": "ß",
+  "{\\o}": "ø",
+  "{\\O}": "Ø",
+  "{\\ae}": "æ",
+  "{\\AE}": "Æ",
+  "{\\oe}": "œ",
+  "{\\OE}": "Œ",
+  "{\\aa}": "å",
+  "{\\AA}": "Å",
+  "{\\l}": "ł",
+  "{\\L}": "Ł",
+  // Add more mappings as needed
+};
+
+function replaceLatexSpecialChars(bibtexEntry) {
+  for (const [latexCode, unicodeChar] of Object.entries(latexToUnicode)) {
+    const regex = new RegExp(latexCode.replace(/[{}]/g, '\\$&'), 'g');
+    bibtexEntry = bibtexEntry.replace(regex, unicodeChar);
+  }
+  return bibtexEntry;
+}
+
+async function fetchPublicationsIrisa() {
+  const url = 'https://api.archives-ouvertes.fr/search/?q=structId_i:490899&wt=bibtex&rows=10000';
+  const response = await axios.get(url, { responseType: 'text' });
+  const bibtexData = response.data;
+
+  const parsedEntries = bibtexParse.entries(bibtexData);
+
+  const publications = [];
+
+  for (const entry of parsedEntries) {
+    if (!entry.ADDRESS || entry.ADDRESS.trim() === '') {
+      continue;
+    }
+
+    const authors = entry.AUTHOR.split(' and ').map(author => {
+      const [lastName, firstName] = author.split(',').map(part => part.trim());
+      return replaceLatexSpecialChars(`${firstName} ${lastName}`);
+    });
+
+    publications.push({
+      title: replaceLatexSpecialChars(entry.TITLE),
+      authors: authors,
+      conference: entry.BOOKTITLE || entry.JOURNAL,
+      year: entry.YEAR,
+      location: replaceLatexSpecialChars(entry.ADDRESS),
+      id: entry.HAL_ID,
+      uri: entry.URL,
+    });
+  }
+
+  // Save the publications to a JSON file
+  fs.writeFileSync('publications.json', JSON.stringify(publications, null, 2));
+  console.log('Publications saved to publications.json');
+}
+
+fetchPublicationsIrisa().then(() => {
+  console.log('Publications fetched and saved to JSON file');
+});
